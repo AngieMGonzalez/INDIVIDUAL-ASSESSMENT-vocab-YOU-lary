900cfbf9f04230804bd356e6e6b8ee8b81a894c2
Angie Gonzalez <angiem.go@gmail.com>
2022-12-03 01:30:59 -0600

    create and read are functional

--- components/forms/selectLang.js
+++ components/forms/selectLang.js
@@ -2,7 +2,7 @@ import renderToDOM from '../../utils/renderToDom';
 
 const selectLang = () => {
   const domString = `<label for="language">Select Language/Technology</label>
-    <select class="form-control" id="category" required>
+    <select class="form-control" id="langTech" required>
     <option value="">Select Language/Tech</option>
     <option value="JavaScript">JavaScript</option>
     <option value="CSS">CSS</option>
