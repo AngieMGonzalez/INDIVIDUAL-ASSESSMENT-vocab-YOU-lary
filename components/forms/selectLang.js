a3a039c277a8092baa983005cd72deb73aaf22be
Angie Gonzalez <angiem.go@gmail.com>
2022-12-03 14:32:14 -0600

    crudded just working on dropdown menue selectLang

--- components/forms/selectLang.js
+++ components/forms/selectLang.js
@@ -2,9 +2,9 @@ import renderToDOM from '../../utils/renderToDom';
 
 const selectLang = () => {
   const domString = `<label for="language">Select Language/Technology</label>
-    <select class="form-control" id="langTech" required>
-    <option value="">Select Language/Tech</option>
-    <option value="JavaScript">JavaScript</option>
+    <select class="form-control" id="langTech"required>
+    <option value="" selected disabled>Select Language/Tech</option>
+    <option value="JavaScript" >JavaScript</option>
     <option value="CSS">CSS</option>
     <option value="HTML">HTML</option>
     `;
